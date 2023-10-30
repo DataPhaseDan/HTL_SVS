27e8f172e29b3a1a2e5331f7b165d0bae5584037
DataPhaseDan <19226450+DataPhaseDan@users.noreply.github.com>
2023-10-30 16:07:28 +0100

    Implement_forms_11

--- src/App.tsx
+++ src/App.tsx
@@ -47,23 +47,26 @@ function App() {
   };
   return (
     <Container className="p-5 border" style={{ backgroundColor: "whitesmoke" }}>
-      <h2>Anmeldefortschritt</h2>
-      <ProgressBar animated now={30} label={`33%`} />
-      <Row>
-        <h2 className="mt-5 mb-5">Anmeldung an der HTL für Berufstätige</h2>
-        <h3 className="mb-5">Schuljahr {currentDate}</h3>
-        <p>
-          <strong>Sie können sich nur einmal anmelden!</strong>
-        </p>
+      <Row className="justify-content-center">
+        <Col xs={8} >
+          <h2>Anmeldefortschritt</h2>
+          <ProgressBar animated now={30} label={`33%`} />
 
-        <p className="h6">
-          Für die Anmeldung sind die Abschlusszeugnisse ihrer bisherigen
-          Ausbildungen notwendig.
-        </p>
-        <Form validated={validated} onSubmit={handleSubmit}>
-          <Col>
-            <Row className="mb-3 mt-5">
-              <Form.Group as={Col} controlId="validationVorname">
+          <h2 className="mt-5 mb-5">Anmeldung an der HTL für Berufstätige</h2>
+          <h3 className="mb-5">Schuljahr {currentDate}</h3>
+          <p>
+            <strong>Sie können sich nur einmal anmelden!</strong>
+          </p>
+
+          <p className="h6">
+            Für die Anmeldung sind die Abschlusszeugnisse ihrer bisherigen
+            Ausbildungen notwendig.
+          </p>
+          <Form validated={validated} onSubmit={handleSubmit}>
+
+
+            <Row className="mb-2 mt-4 ">
+              <Form.Group controlId="validationVorname">
                 <FloatingLabel
                   controlId="formVorname"
                   label="Vorname"
@@ -74,13 +77,13 @@ function App() {
                     placeholder="Vorname"
                     pattern="[A-Z][a-z]*"
                   />
-                  <Form.Control.Feedback type="invalid" className="mx-2">
+                  <Form.Control.Feedback type="invalid" className="mx-2 mb-3">
                     Bitte geben Sie den Vornamen des Bewerbers an.
                   </Form.Control.Feedback>
                 </FloatingLabel>
               </Form.Group>
 
-              <Form.Group as={Col} controlId="validationNachname">
+              <Form.Group controlId="validationNachname">
                 <FloatingLabel
                   controlId="formNachname"
                   label="Nachname"
@@ -97,9 +100,9 @@ function App() {
                 </FloatingLabel>
               </Form.Group>
             </Row>
-          </Col>
 
-          <Col lg={8}>
+
+
             <Form.Group
               as={Col}
               className="mt-5 mb-3"
@@ -113,7 +116,7 @@ function App() {
                   required
                   type="text"
                   pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/"
-                  //placeholder="ihre@email.hier"
+                //placeholder="ihre@email.hier"
                 />
                 <Form.Control.Feedback type="invalid" className="mx-2">
                   Bitte geben Sie die E-mail des Bewerbers an.
@@ -148,7 +151,7 @@ function App() {
                   // placeholder="+43 123 456 7890"
                   value={phone}
                   onChange={handlePhoneChange}
-                  // isInvalid={!isPhoneValid()}
+                // isInvalid={!isPhoneValid()}
                 />
                 <Form.Control.Feedback type={isPhoneValid()} className="mx-2">
                   Bitte geben Sie die Tel. Nr. des Bewerbers im Format +XX XXX
@@ -167,164 +170,169 @@ function App() {
                 </Container>
               </FloatingLabel>
             </Form.Group>
-          </Col>
 
-          <Form.Group
-            as={Col}
-            className="mb-3 mt-5"
-            controlId="validationFachrichtung"
-          >
-            <FloatingLabel
-              controlId="formSelectFachrichtung"
-              label="Bitte wählen Sie die gewünschte Fachrichtung"
-              className="pt-1 mt-3"
+
+            <Form.Group
+              className="mb-3 mt-5"
+              controlId="validationFachrichtung"
             >
-              <Form.Select required>
-                <option></option>
-                <option>Abend-HTL für Berufstätige (Bautechnik)</option>
-                <option>Abend-HTL für Berufstätige (Elektrotechnik)</option>
-                <option>Abend-HTL für Berufstätige (Informatik)</option>
-                <option>Abend-HTL für Berufstätige (Maschinenbau)</option>
-              </Form.Select>
-              <Form.Control.Feedback type="invalid" className="mx-2">
-                Ihre 1. Wahl:
-              </Form.Control.Feedback>
-            </FloatingLabel>
-          </Form.Group>
-
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="validationGeburtsdatum">
               <FloatingLabel
-                controlId="formGridState"
-                label="Geburtsdatum"
-                className="pt-1 mt-3"
+                controlId="formSelectFachrichtung"
+                label="Fachrichtung"
+                className="mt-5"
               >
-                {/* <Form.Label htmlFor="inputBirthDate">Geburtsdatum</Form.Label> */}
-                <Form.Control
-                  type="date"
-                  id="inputBirthDate"
-                  required
-                  title="Bitte geben Sie ihr Geburtsdatum ein."
-                />
+                <Form.Select required>
+                  <option></option>
+                  <option>Abend-HTL für Berufstätige (Bautechnik)</option>
+                  <option>Abend-HTL für Berufstätige (Elektrotechnik)</option>
+                  <option>Abend-HTL für Berufstätige (Informatik)</option>
+                  <option>Abend-HTL für Berufstätige (Maschinenbau)</option>
+                </Form.Select>
                 <Form.Control.Feedback type="invalid" className="mx-2">
-                  Bitte wählen Sie das Geburtsdatum des Bewerbers.
+                  Ihre 1. Wahl:
                 </Form.Control.Feedback>
               </FloatingLabel>
             </Form.Group>
-          </Row>
 
-          <Form.Group className="mb-3" id="formGridCheckbox">
-            <Accordion>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>Datenschutzgrundverordnung</Accordion.Header>
-                <Accordion.Body>
-                  <Container
-                    className="mt-2 mb-2 p-3 border 5px solid black"
-                    style={{ whiteSpace: "pre-wrap" }}
-                  >
-                    <Form.Text id="DsgvoState" muted>
-                      <p className="h5">
-                        Datenschutzgrundverordnung
-                      </p>
-                      <p className="h6 mb-4">
-                        Einwilligung Art. 7 DSGVO
-                      </p>
-                      <p>
-                        <em>
-                          Ich bin damit einverstanden, dass die angeführten
-                          personenbezogenen Daten ausschließlich für schulische
-                          und organisatorische Zwecke teilweise oder vollständig
-                          verarbeitet werden.
-                        </em>
-                      </p>
-                      <p className="h6">
-                        Speicherdauer:
-                      </p>
-                      {/* <br className='my-1' /> */}
-                      <p>
-                        <em>
-                          Die personenbezogenen Daten werden von der HTBLuVA
-                          Salzburg nur so lange gespeichert, wie es unter
-                          Einhaltung der einschlägigen gesetzlichen Bestimmungen
-                          zur Erfüllung des jeweils genannten Zwecks notwendig
-                          ist, oder solange gespeichert, als gesetzliche
-                          Aufbewahrungsfristen bestehen oder Verjährungsfristen
-                          betreffen potentieller Rechtsansprüche noch offen
-                          sind.
-                        </em>
-                      </p>
+            <Row className="mb-5">
+              <Form.Group controlId="validationGeburtsdatum">
+                <FloatingLabel
+                  controlId="formGridState"
+                  label="Geburtsdatum"
+                  className="pt-1 mt-3"
+                >
+                  {/* <Form.Label htmlFor="inputBirthDate">Geburtsdatum</Form.Label> */}
+                  <Form.Control
+                    type="date"
+                    id="inputBirthDate"
+                    required
+                    title="Bitte geben Sie ihr Geburtsdatum ein."
+                  />
+                  <Form.Control.Feedback type="invalid" className="mx-2">
+                    Bitte wählen Sie das Geburtsdatum des Bewerbers.
+                  </Form.Control.Feedback>
+                </FloatingLabel>
+              </Form.Group>
+            </Row>
+         
+              <Form.Group className="mb-3" id="formGridCheckbox">
+                <Accordion>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header className='text-break'>Datenschutzgrundverordnung</Accordion.Header>
+                    <Accordion.Body className="text-wrap">
+                      <Container
+                        className="mt-2 mb-2 p-3 border 5px solid black"
+                        style={{  wordWrap:"break-word", whiteSpace: "pre-wrap",overflowWrap: "break-word" }}
+                      >
+                        <Form.Text id="DsgvoState" muted>
+                          <p className="h5">
+                            Datenschutzgrundverordnung
+                          </p>
+                          <p className="h6 mb-4">
+                            Einwilligung Art. 7 DSGVO
+                          </p>
+                          <p>
+                            <em>
+                              Ich bin damit einverstanden, dass die angeführten
+                              personenbezogenen Daten ausschließlich für schulische
+                              und organisatorische Zwecke teilweise oder vollständig
+                              verarbeitet werden.
+                            </em>
+                          </p>
+                          <p className="h6">
+                            Speicherdauer:
+                          </p>
+                          {/* <br className='my-1' /> */}
+                          <p>
+                            <em>
+                              Die personenbezogenen Daten werden von der HTBLuVA
+                              Salzburg nur so lange gespeichert, wie es unter
+                              Einhaltung der einschlägigen gesetzlichen Bestimmungen
+                              zur Erfüllung des jeweils genannten Zwecks notwendig
+                              ist, oder solange gespeichert, als gesetzliche
+                              Aufbewahrungsfristen bestehen oder Verjährungsfristen
+                              betreffen potentieller Rechtsansprüche noch offen
+                              sind.
+                            </em>
+                          </p>
 
-                      <p className="h6">
-                        Als personenbezogene Daten werden verarbeitet:
-                      </p>
-                      {/* <br /> */}
-                      <p>
-                        <em>
-                          Vor- und Zuname der/des Studierenden, Wohnadresse,
-                          Geburtsdatum, IP-Adressen und Kontaktdaten des
-                          Internetproviders, Versicherungsnummer,
-                          Religionsbekenntnis, Muttersprache,
-                          Staatsbürgerschaft, Telefonnummern.
-                        </em>
-                      </p>
+                          <p className="h6">
+                            Als personenbezogene Daten werden verarbeitet:
+                          </p>
+                          {/* <br /> */}
+                          <p>
+                            <em>
+                              Vor- und Zuname der/des Studierenden, Wohnadresse,
+                              Geburtsdatum, IP-Adressen und Kontaktdaten des
+                              Internetproviders, Versicherungsnummer,
+                              Religionsbekenntnis, Muttersprache,
+                              Staatsbürgerschaft, Telefonnummern.
+                            </em>
+                          </p>
 
-                      <p className="h6">
-                        Verwendungszwecke für die personenbezogene
-                        Datenverarbeitung sind:
-                        {/* <br /> */}
-                      </p>
-                      <p>
-                        <em>
-                          WebUntis inkl. Fotos (elektronisches Tagebuch),
-                          Klassenlisten, Schulbuchlisten, AUVA Meldungen, Netz-
-                          und E-Mail-Account, elektronische Zutrittssysteme,
-                          Schulgeldverwaltung (edu.PAY), Schulfotografie,
-                          Bezirksverwaltungsbehörden, Magistrat, Polizei.
-                        </em>
-                      </p>
-                      <p>
-                        <em>
-                          Zudem gebe ich die Einwilligung, dass Fotos bzw.
-                          Videos veröffentlicht werden dürfen (z.B. Homepage der
-                          Schule, Jahresbericht der Schule, lokale Medien und
-                          Berichte in Tageszeitungen, Rundfunk und Fernsehen).
-                        </em>
-                      </p>
-                      <p>
-                        <em>
-                          Dem Unterfertigten steht grundsätzlich das Recht auf
-                          Auskunft, Berechtigung, Löschung, Einschränkung,
-                          Datenübertragbarkeit, Widerruf und Widerspruch zu.
-                          Diese Rechte können gegenüber der HTBLuVA Salzburg
-                          geltend gemacht werden, wenn die Verarbeitung der
-                          personenbezogenen Daten gegen geltendes
-                          Datenschutzrecht verstößt oder datenschutzrechtliche
-                          Ansprüche sonst in einer Weise verletzt worden sind.
-                          Dann ist eine Beschwerde an die Österreichische
-                          Datenschutzbehörde als zuständige Aufsichtsbehörde
-                          gemäß Art. 77 DSGVO zu erheben. Durch den Widerruf der
-                          Einwilligung wird die Rechtmäßigkeit der aufgrund der
-                          Einwilligung bis zum Widerruf erfolgten Verarbeitung
-                          nicht berührt.
-                        </em>
-                      </p>
-                    </Form.Text>
-                  </Container>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-            <Form.Check
-              required
-              type="checkbox"
-              label="Ich stimme der Datenschutzgrundverordnung der HTBLuVA Salzburg zu."
-            />
-          </Form.Group>
+                          <p className="h6">
+                            Verwendungszwecke für die personenbezogene
+                            Datenverarbeitung sind:
+                            {/* <br /> */}
+                          </p>
+                          <p>
+                            <em>
+                              WebUntis inkl. Fotos (elektronisches Tagebuch),
+                              Klassenlisten, Schulbuchlisten, AUVA Meldungen, Netz-
+                              und E-Mail-Account, elektronische Zutrittssysteme,
+                              Schulgeldverwaltung (edu.PAY), Schulfotografie,
+                              Bezirksverwaltungsbehörden, Magistrat, Polizei.
+                            </em>
+                          </p>
+                          <p>
+                            <em>
+                              Zudem gebe ich die Einwilligung, dass Fotos bzw.
+                              Videos veröffentlicht werden dürfen (z.B. Homepage der
+                              Schule, Jahresbericht der Schule, lokale Medien und
+                              Berichte in Tageszeitungen, Rundfunk und Fernsehen).
+                            </em>
+                          </p>
+                          <p>
+                            <em>
+                              Dem Unterfertigten steht grundsätzlich das Recht auf
+                              Auskunft, Berechtigung, Löschung, Einschränkung,
+                              Datenübertragbarkeit, Widerruf und Widerspruch zu.
+                              Diese Rechte können gegenüber der HTBLuVA Salzburg
+                              geltend gemacht werden, wenn die Verarbeitung der
+                              personenbezogenen Daten gegen geltendes
+                              Datenschutzrecht verstößt oder datenschutzrechtliche
+                              Ansprüche sonst in einer Weise verletzt worden sind.
+                              Dann ist eine Beschwerde an die Österreichische
+                              Datenschutzbehörde als zuständige Aufsichtsbehörde
+                              gemäß Art. 77 DSGVO zu erheben. Durch den Widerruf der
+                              Einwilligung wird die Rechtmäßigkeit der aufgrund der
+                              Einwilligung bis zum Widerruf erfolgten Verarbeitung
+                              nicht berührt.
+                            </em>
+                          </p>
+                        </Form.Text>
+                      </Container>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+          
+              </Form.Group>
+              <Form.Check
+                  required
+                  type="checkbox"
+                  label="Ich stimme der Datenschutzgrundverordnung der HTBLuVA Salzburg zu."
+                  className="mb-3"
+                />
 
-          <Button variant="success" type="submit">
-            Bestätigen
-          </Button>
-        </Form>
+            <Button variant="success" type="submit">
+              Bestätigen
+            </Button>
+
+
+          </Form>
+        </Col>
       </Row>
+
     </Container>
   );
 }
