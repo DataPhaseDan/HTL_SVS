e7692eac933aa2b1deeef0710c6bfa802f9619a7
DataPhaseDan <19226450+DataPhaseDan@users.noreply.github.com>
2024-02-06 16:40:09 +0100

    appendix final stage 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import Adminpanel from "./adminpanel";
- import Abendschule1 from './abendschule1.tsx'
+import Abendschule1 from './abendschule1.tsx'
 //import './index.css'
 import Abendschule2 from './abendschule2'
 import Tagesschule1 from "./tagesschule1";
@@ -12,8 +12,9 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
-			<Abendschule1/>
-			<Abendschule2/>
+			<Abendschule1 />
+			<Abendschule2 />
+			<Tagesschule1 />
 			<Tagesschule2 />
 			<Adminpanel />
 		</React.StrictMode>,
